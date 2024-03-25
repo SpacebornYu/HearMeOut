f207040418a18da1842fca3ea71c53d7f922bfcf
SpacebornYu <120189548+SpacebornYu@users.noreply.github.com>
2024-03-25 11:38:23 +0100

    .

---
+++ HearMeOut/HearMeOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  HearMeOutApp.swift
+//  HearMeOut
+//
+//  Created by Yuri Mario Gianoli on 22/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HearMeOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
