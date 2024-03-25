75e1a39f2f571b4c9cc3794a4072488c5038f555
SpacebornYu <120189548+SpacebornYu@users.noreply.github.com>
2024-03-25 14:44:42 +0100

    .

--- VideoView.swift
+++ VideoView.swift
@@ -1,50 +1,50 @@
+////
+////  VideoView.swift
+////  HearMeOut
+////
+////  Created by Yuri Mario Gianoli on 25/03/24.
+////
 //
-//  VideoView.swift
-//  HearMeOut
+//import SwiftUI
 //
-//  Created by Yuri Mario Gianoli on 25/03/24.
+//struct VideoView: View {
+//    @Environment(\.dismiss) var dismiss
+//    
+//    var body: some View {
+//        
+//        NavigationStack {
+//            VStack() {
+//                if Platform.isSimulator {
+//                  
+//                    Text("Oops! You are using the Simulator 👀. \nRun the App on iPhone to use the \nlive Camera.").multilineTextAlignment(.center).padding()
+//                }else{
+//                    SwiftUIAVCaptureVideoPreviewView()
+//                }
+//            }
+//                .navigationTitle("Camera")
+//                .toolbar {
+//                    ToolbarItem(placement: .navigationBarTrailing) {
+//                        Button(action: {
+//                            dismiss()
+//                        }) {
+//                            Image(systemName: "x.circle").padding(.top)
+//                        }
+//                    }
+//                }
+//        }
+//    }
+//}
 //
-
-import SwiftUI
-
-struct VideoView: View {
-    @Environment(\.dismiss) var dismiss
-    
-    var body: some View {
-        
-        NavigationStack {
-            VStack() {
-                if Platform.isSimulator {
-                  
-                    Text("Oops! You are using the Simulator 👀. \nRun the App on iPhone to use the \nlive Camera.").multilineTextAlignment(.center).padding()
-                }else{
-                    SwiftUIAVCaptureVideoPreviewView()
-                }
-            }
-                .navigationTitle("Camera")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button(action: {
-                            dismiss()
-                        }) {
-                            Image(systemName: "x.circle").padding(.top)
-                        }
-                    }
-                }
-        }
-    }
-}
-
-#Preview {
-    VideoView().preferredColorScheme(.dark)
-}
-
-// This block is used in order to detect if the app is running on a simulator or not. In this case, the detection will prevent a crash of the app during the preview of the live camera.
-
-struct Platform {
-
-    static var isSimulator: Bool {
-        return TARGET_OS_SIMULATOR != 0
-    }
-
-}
+//#Preview {
+//    VideoView().preferredColorScheme(.dark)
+//}
+//
+//// This block is used in order to detect if the app is running on a simulator or not. In this case, the detection will prevent a crash of the app during the preview of the live camera.
+//
+//struct Platform {
+//
+//    static var isSimulator: Bool {
+//        return TARGET_OS_SIMULATOR != 0
+//    }
+//
+//}
